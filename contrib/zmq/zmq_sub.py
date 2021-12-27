3d64bcccf3f0913b2e2b3d9f37d43887ecf24547
ronaldopadula <ronaldopadula@gmail.com>
2021-12-26 21:42:31 -0300

    alteração diretório contrib com termos Litecoin, litecoin e LTC

--- contrib/zmq/zmq_sub.py
+++ contrib/zmq/zmq_sub.py
@@ -6,8 +6,8 @@
 """
     ZMQ example using python3's asyncio
 
-    Litecoind should be started with the command line arguments:
-        litecoind -testnet -daemon \
+    RPOcoind should be started with the command line arguments:
+        rpocoind -testnet -daemon \
                 -zmqpubrawtx=tcp://127.0.0.1:28332 \
                 -zmqpubrawblock=tcp://127.0.0.1:28332 \
                 -zmqpubhashtx=tcp://127.0.0.1:28332 \
